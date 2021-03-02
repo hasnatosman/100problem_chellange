99bb2db95802beea48af0c95b40234331d264132
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-03-02 12:34:34 +0600

    Add files via upload

---
+++ sor-.py
@@ -0,0 +1,21 @@
+"""
+Question:
+Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically.
+Suppose the following input is supplied to the program:
+hello world and practice makes perfect and hello world again
+Then, the output should be:
+again and hello makes perfect practice world
+
+"""
+"""
+
+s = input()
+words = [word for word in s.split(' ')]
+print(''.join(sorted(list(set(words)))))
+
+"""
+s = input('Input some words: ')
+words = []
+for word in s:
+    words.append(word)
+print(''.join(sorted(list(set(words)))))
